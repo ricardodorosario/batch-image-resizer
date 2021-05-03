09c78b50930c866f1ddb9ff3f6e1e1188fdc0c10
Ricardo do Rosario <rdr.rosario@yahoo.com.br>
2021-05-03 11:49:18 -0300

    arguments and extension adjustments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # batch-image-resizer
 A shell script that resizes all the images of a folder recursively.
+
+Usage:
+
+[code]./batch-image-resizer.sh -d /path/to/root/directory -s 1000[/code]
--- batch-image-resizer.sh
+++ batch-image-resizer.sh
@@ -1,22 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-# the directory that the script will work
-directory="/home/user/Desktop/"
-# the extension
-extension="*.jpg"
-# max size of the file
-max=1000
-#permission after resize
-permission="ricardo:ricardo"
+#Example: ./batch-image-resizer.sh -d /path/to/root/directory -s 1000
 
-find "$directory" -type f -name "$extension" | 
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -d /path/to/root/dir -s maxSize"
+   exit 1
+}
+
+while getopts "d:s:" opt
+do
+   case "$opt" in
+      d ) directory="$OPTARG" ;;
+      s ) max="$OPTARG" ;;
+      ? ) helpFunction ;;
+   esac
+done
+
+if [ -z "$directory" ] || [ -z "$max" ]
+then
+   echo "Some of the parameters are empty";
+   helpFunction
+fi
+
+# the extensions
+extensions=".*\(jpg\|png\|jpeg\)$"
+
+find "$directory" -regex "$extensions" | 
 while read dir
 	do
 		width=$(identify -format "%w" "$dir")
 		height=$(identify -format "%h" "$dir")
 		if [ \( $width -gt $max \) -o \( $height -gt $max \) ]
 			then
-				echo "IS BIG $width x $height $dir"
+				echo "BIGGER THAN EXPECTED\n$width x $height\n$dir"
+				permission=$(find "$dir" -printf "%u:%g")
 				mogrify -resize 50% -quality 90 "$dir"
 				chown $permission "$dir"
 				echo $(identify -format "%w x %h" "$dir")
